5e3fb1f0f8da0b5ca558afd84d8b9df0d810682c
Ira Miller <public@iramiller.com>
2018-07-01 09:51:40 -0500

    fork guld-spawn

--- keyring.js
+++ keyring.js
@@ -1,43 +1,15 @@
-const spawn = require('child_process').spawn
 const pify = require('pify')
 const rimraf = pify(require('rimraf'))
 const fs = pify(require('fs'))
 const fpre = /[A-Z0-9]{40}/gm
 const { homedir, tmpdir } = require('os')
 const path = require('path')
+const spawn = require('guld-spawn')
 // set GNUPGHOME to default if it isn't already
 if (!process.env.hasOwnProperty('GNUPGHOME')) process.env.GNUPGHOME = path.join(homedir(), '.gnupg')
 
 function getGpgAgentRe (grip) {
   return new RegExp(`^S KEYINFO ${grip} [DP1 -]{0,20}\nOK\n*$`)
-}
-
-async function scall (command, stdin, args = [], redirectErr = false) {
-  return new Promise((resolve, reject) => {
-    const proc = spawn(command, args)
-    const buffers = []
-    let buffersLength = 0
-    let stderr = ''
-    proc.stdout.on('data', function (buf) {
-      buffers.push(buf)
-      buffersLength += buf.length
-    })
-    proc.stderr.on('data', function (buf) {
-      if (!redirectErr) {
-        stderr += buf.toString('utf8')
-      } else {
-        buffers.push(buf)
-        buffersLength += buf.length
-      }
-    })
-    proc.on('close', function (code) {
-      var stdout = Buffer.concat(buffers, buffersLength)
-      if (code !== 0 && stderr !== '') reject(new Error(stderr))
-      stdout = stdout.toString('utf-8')
-      resolve(stdout)
-    })
-    proc.stdin.end(stdin)
-  })
 }
 
 async function generate (options) {
@@ -55,7 +27,7 @@ Expire-Date: 0
     opstr = `${opstr}Passphrase: ${options.passphrase}\n`
   }
   opstr = `${opstr}%commit\n`
-  var resp = await scall('gpg2', opstr, ['--batch', '--generate-key'], true)
+  var resp = await spawn('gpg2', opstr, ['--batch', '--generate-key'], true)
   var r = resp.match(/key [A-Z0-9]{16} marked as ultimately trusted/)
   if (r === null) throw new Error(`Unable to generate key, output ${resp}`)
   r = r[0].match(/[A-Z0-9]{16}/)
@@ -74,12 +46,12 @@ async function clear () {
 }
 
 async function store () {
-  await scall(`gpg2`, '', ['--list-public-keys'], true)
+  await spawn(`gpg2`, '', ['--list-public-keys'], true)
   await fs.mkdir(path.join(process.env.GNUPGHOME, 'private-keys-v1.d'), 0o700)
 }
 
 async function getKeygrips (fpr) {
-  var data = await scall('gpg2', '', ['--fingerprint', '--with-keygrip', fpr])
+  var data = await spawn('gpg2', '', ['--fingerprint', '--with-keygrip', fpr])
   return data.toString('utf-8').match(fpre)
 }
 
@@ -87,7 +59,7 @@ async function listKeys (fpr) {
   var keys = {}
   var args = ['--list-keys', '--with-colons']
   if (fpr) args.push(fpr)
-  var data = await scall('gpg2', '', args)
+  var data = await spawn('gpg2', '', args)
   var da = data.toString('utf-8')
   da = da.match(/fpr.*[\n]+.*uid.*::::::::::/g)
   if (da) {
@@ -105,7 +77,7 @@ async function listSecretKeys (term) {
   var keys = {}
   var args = ['--list-secret-keys', '--with-colons']
   if (term) args.push(term)
-  var data = await scall('gpg2', '', args)
+  var data = await spawn('gpg2', '', args)
   var da = data.toString('utf-8')
   da = da.match(/sec.*[\n]+.*[A-Z0-9]{40}.*[\n]*.*[\n]*.*uid.*::::::::::/g)
   if (da) {
@@ -120,7 +92,7 @@ async function listSecretKeys (term) {
 }
 
 async function importPublicKey (key) {
-  await scall('gpg2', key, ['--batch', '--import'], true)
+  await spawn('gpg2', key, ['--batch', '--import'], true)
 }
 
 async function importSecretKey (key, pass) {
@@ -130,18 +102,18 @@ async function importSecretKey (key, pass) {
     args.unshift('--passphrase')
     args.unshift('--batch')
   }
-  var resp = await scall('gpg2', key, args, true)
+  var resp = await spawn('gpg2', key, args, true)
   var r = resp.match(/key [A-Z0-9]{16}: secret key imported/)
   if (r === null) throw new Error(`Unable to import key, output ${resp}`)
   r = r[0].match(/[A-Z0-9]{16}/)
   var keys = await listKeys(r[0])
   var fpr = keys[Object.keys(keys)[0]]
-  await scall('gpg2', 'trust\n5\ny\nsave\n', ['--command-fd=0', '--edit-key', fpr], true)
+  await spawn('gpg2', 'trust\n5\ny\nsave\n', ['--command-fd=0', '--edit-key', fpr], true)
   return fpr
 }
 
 async function getPublicKey (fpr) {
-  return scall('gpg2', '', ['-a', '--export', fpr])
+  return spawn('gpg2', '', ['-a', '--export', fpr])
 }
 
 async function getSecretKey (fpr, pass) {
@@ -151,12 +123,12 @@ async function getSecretKey (fpr, pass) {
     args.unshift('--passphrase')
     args.unshift('--batch')
   }
-  return scall('gpg2', '', args)
+  return spawn('gpg2', '', args)
 }
 
 async function isLocked (fpr) {
   const keygrips = await getKeygrips(fpr)
-  const stdout = await scall('gpg-connect-agent', `KEYINFO --no-ask ${keygrips[0]} Err Pmt Des`)
+  const stdout = await spawn('gpg-connect-agent', `KEYINFO --no-ask ${keygrips[0]} Err Pmt Des`)
   return !getGpgAgentRe(keygrips[0]).test(stdout)
 }
 
@@ -167,22 +139,22 @@ async function unlockKey (fpr, pass) {
     args.unshift('--passphrase')
     args.unshift('--batch')
   }
-  if (await this.isLocked(fpr)) await scall('gpg2', 'hello', args)
+  if (await this.isLocked(fpr)) await spawn('gpg2', 'hello', args)
 }
 
 async function lockKey (fpr, pass) {
-  await scall('gpg-connect-agent', '', ['reloadagent', '/bye'])
+  await spawn('gpg-connect-agent', '', ['reloadagent', '/bye'])
 }
 
 async function sign (message, fpr) {
-  return scall('gpg2', message, ['--detach-sign', '-a', '-u', fpr])
+  return spawn('gpg2', message, ['--detach-sign', '-a', '-u', fpr])
 }
 
 async function verify (message, signature, signers) {
   if (!Array.isArray(signers)) signers = [signers]
   var tmpfile = path.join(tmpdir(), `${Date.now()}.sig`)
   await fs.writeFile(tmpfile, signature)
-  var res = await scall('gpg2', message, ['--status-fd=1', '--verify', tmpfile, '-'], true)
+  var res = await spawn('gpg2', message, ['--status-fd=1', '--verify', tmpfile, '-'], true)
   await fs.unlink(tmpfile)
   // TODO check response against signers
   var active
@@ -211,35 +183,35 @@ async function verify (message, signature, signers) {
 }
 
 async function decrypt (message) {
-  return scall('gpg2', message, ['-d'])
+  return spawn('gpg2', message, ['-d'])
 }
 
 async function encrypt (message, fpr) {
-  return scall('gpg2', message, ['-a', '-e', '-r', fpr])
+  return spawn('gpg2', message, ['-a', '-e', '-r', fpr])
 }
 
 async function decryptFile (i) {
-  return scall('gpg2', '', ['-d', i])
+  return spawn('gpg2', '', ['-d', i])
 }
 
 async function encryptFile (i, fpr) {
-  return scall('gpg2', '', ['-a', '-e', '-r', fpr, i])
+  return spawn('gpg2', '', ['-a', '-e', '-r', fpr, i])
 }
 
 async function decryptToFile (message, i) {
-  return scall('gpg2', message, ['-o', i, '-d'])
+  return spawn('gpg2', message, ['-o', i, '-d'])
 }
 
 async function encryptToFile (message, i, fpr) {
-  return scall('gpg2', message, ['-o', i, '-a', '-e', '-r', fpr])
+  return spawn('gpg2', message, ['-o', i, '-a', '-e', '-r', fpr])
 }
 
 async function decryptFileToFile (i, o) {
-  return scall('gpg2', '', ['-o', o, '-d', i])
+  return spawn('gpg2', '', ['-o', o, '-d', i])
 }
 
 async function encryptFileToFile (i, o, fpr) {
-  return scall('gpg2', '', ['-o', o, '-a', '-e', '-r', fpr, i])
+  return spawn('gpg2', '', ['-o', o, '-a', '-e', '-r', fpr, i])
 }
 
 module.exports = {
